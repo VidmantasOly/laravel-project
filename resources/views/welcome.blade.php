39aac98e3e041d4d5e4137eae5bacb1f4528d479
VidmantasG <vidmantas.gutauskas@oc.eu>
2017-12-05 15:14:21 +0200

    Set up build process

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravalėl
                 </div>
 
                 <div class="links">
